e3d2ceffcf9349514cd0ba4588f6a22bfaa2d3bc
vladislav.tagayev <vladislav.tagayev@nedra.digital>
2021-04-05 18:15:05 +0300

    feat(graph): add template fields

--- handlers.go
+++ handlers.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"net/http"
+	"sa-back/graph"
+	"sa-back/graph/generated"
+)
+
+func gqlHandler() *handler.Server {
+	return handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+}
 
 func handlers(mux *http.ServeMux) {
+	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mux.Handle("/query", gqlHandler())
 	mux.HandleFunc("/upload", uploadHandler)
 }
